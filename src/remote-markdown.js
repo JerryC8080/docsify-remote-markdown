76fe56aa6589fe09e794fc6b168f1083c64a214a
zk <liuzq7@gmail.com>
2020-08-01 00:54:12 +0800

    support mutipal markdown file

--- src/remote-markdown.js
+++ src/remote-markdown.js
@@ -1,29 +1,46 @@
 export function getFile(target) {
-  return Promise
-    .resolve()
+  return Promise.resolve()
     .then(() => fetch(target))
     .then((response) => {
       if (!response.ok) {
-        throw new Error('HTTP error, status = ' + response.status);
+        throw new Error("HTTP error, status = " + response.status);
       }
       return response.text();
-    })
+    });
 }
 
 export function install(hook, vm) {
-  const config = Object.assign({}, {
-		tag: 'remoteMarkdownUrl',
-	}, vm.config.remoteMarkdown);
+  const config = Object.assign(
+    {},
+    {
+      tag: "remoteMarkdownUrl",
+    },
+    vm.config.remoteMarkdown
+  );
 
   hook.beforeEach(function (content, next) {
-    const reg = new RegExp(`\\[${config.tag}\\]\\((http|https://.+)\\)`);
-    const result = content.match(reg);
+    //const reg = new RegExp(
+      //`(?<=\\[${config.tag}\\]\\()(http|https://.+)(?=\\))`,
+      //"g"
+    //);
+    const markdownTagsRegex = new RegExp(
+      `\\[${config.tag}\\]\\((http|https://.+)\\)`,
+      "g"
+    );
 
-    if (result && result[1]) {
-      const targetFile = result[1];
-      getFile(targetFile)
-        .then((data) => next(content.replace(reg, `\n ${data} \n`)))
-        .catch((err) => console.error(err));
+    const tags = content.match(markdownTagsRegex);
+    console.log("tags", tags[0]);
+    if (tags && tags.length > 0) {
+      Promise.all(
+        tags.map((tag) => fetch(tag.slice(tag.indexOf("(")+1,tag.indexOf(")"))).then((resp) => resp.text()))
+      ).then((texts) => {
+        texts.forEach((data, idx) => {
+          console.log(idx, "->", tags[idx]);
+          console.log("data->", data);
+          content = content.replace(tags[idx], `\n ${data} \n`);
+        });
+        next(content);
+      });
     } else {
       next(content);
     }
